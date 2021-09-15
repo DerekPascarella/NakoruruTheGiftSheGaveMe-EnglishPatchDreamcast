dd12ca85903eaf49b44bbcac1627da69dd04926d
Derek Pascarella <contact.pascarella@gmail.com>
2021-09-14 23:33:46 -0400

    Add files via upload

--- custom_tools/script_insert.pl
+++ custom_tools/script_insert.pl
@@ -28,7 +28,6 @@ for(my $i = 1; $i < scalar(@spreadsheet_rows); $i ++)
 {
 	my $line_number_spreadsheet = int($spreadsheet_rows[$i][0]);
 	my $english_text = decode_entities($spreadsheet_rows[$i][3]);
-
 	$english_replacements{$line_number_spreadsheet} = $english_text;
 }
 
@@ -45,112 +44,179 @@ close(FH);
 
 for(my $j = 0; $j < scalar(@textfile_lines); $j ++)
 {
-	if(exists($english_replacements{$j + 1}))
+	if(exists($english_replacements{$j + 1}) && $english_replacements{$j + 1} ne "")
 	{
-		if($english_replacements{$j + 1} ne "")
+		my $temp_hex;
+
+		if($j > 0)
 		{
-			my $temp_hex = "0A" . &generate_hex($english_replacements{$j + 1});
+			$temp_hex .= "0A";
+		}
 
-			if((($english_replacements{$j + 2} eq "" && $english_replacements{$j + 3} eq "" && $textfile_lines[$j + 3] =~ /^wpv/)
-				|| ($english_replacements{$j + 2} eq "" && $textfile_lines[$j + 2] =~ /^wpv/))
-				&& $temp_hex =~ /0D0A6D6968203030300D0A6D726E206E6F0D0A/)
+		$temp_hex .= &generate_hex($english_replacements{$j + 1});
+
+		# print "if \"" . $english_replacements{$j + 2} . "\" eq blank\n";
+		# print "and \"" . $english_replacements{$j + 3} . "\" eq blank\n";
+		# print "and \"";
+		# print $textfile_lines[$j+3];
+		# print "\" starts with \"wpv\"\n";
+		# print "---or---\n";
+		# print "if \"" . $english_replacements{$j + 2} . "\" eq blank\n";
+		# print "and \"";
+		# print $textfile_lines[$j+2];
+		# print "\" starts with \"wpv\"\n";
+		# print "---and---\n";
+		# print "\"" . $temp_hex . "\" contains \"0D0A6D6968203030300D0A6D726E206E6F0D0A\"\n\n-------------------\n\n";
+
+		# print "eng_rep(j+2): ";
+		# print $english_replacements{$j + 2};
+		# print "\neng_rep(j+3): ";
+		# print $english_replacements{$j + 3};
+		# print "\ntextf_ln(j+3): ";
+		# print $textfile_lines[$j + 3];
+		# print "\n--or--\n";
+		# print "eng_rep(j+2): ";
+		# print $english_replacements{$j + 2};
+		# print "\ntextf_ln(j+2): ";
+		# print $textfile_lines[$j + 2];
+		# print "\ntextf_ln(j+1): ";
+		# print $textfile_lines[$j + 1];
+		# print "\ntextf_ln(j): ";
+		# print $textfile_lines[$j];
+		# print "\n-------------\n";
+
+		# if((($english_replacements{$j + 2} eq "" && $english_replacements{$j + 3} eq "" && $textfile_lines[$j + 3] =~ /^wpv/)
+		# 	|| ($english_replacements{$j + 2} eq "" && $textfile_lines[$j + 2] =~ /^wpv/))
+		# 	&& $temp_hex =~ /0D0A6D6968203030300D0A6D726E206E6F0D0A/)
+		# {
+
+		if((($english_replacements{$j + 2} eq "" && $english_replacements{$j + 3} eq "" && $textfile_lines[$j + 3] =~ /^wpv/)
+			|| ($english_replacements{$j + 2} eq "" && $textfile_lines[$j + 2] =~ /^wpv/)
+			|| ($textfile_lines[$j + 1] =~ /^wpv/))
+			&& $temp_hex =~ /0D0A6D6968203030300D0A6D726E206E6F0D0A/)
+		{
+			$line_count_orig ++;
+			my @temp_hex_split = split(/0D0A6D6968203030300D0A6D726E206E6F0D0A/, $temp_hex);
+			$temp_hex = "";
+			
+			#print "--got here--\n";
+
+			for(my $k = 0; $k < scalar(@temp_hex_split); $k ++)
 			{
-				$line_count_orig ++;
-				my @temp_hex_split = split(/0D0A6D6968203030300D0A6D726E206E6F0D0A/, $temp_hex);
-				$temp_hex = "";
+				$temp_hex .= $temp_hex_split[$k];
 
-				for(my $k = 0; $k < scalar(@temp_hex_split); $k ++)
+				if($k == 0)
 				{
-					$temp_hex .= $temp_hex_split[$k];
-
-					if($k == 0)
+					if($textfile_lines[$j + 1] =~ /^wpv/)
 					{
-						if($textfile_lines[$j + 2] =~ /^wpv/)
-						{
-							$temp_hex .= "0D0A" . substr(ascii_to_hex($textfile_lines[$j + 2]), 0, -2);
-							$textfile_lines[$j + 2] = "";
-						}
-						elsif($textfile_lines[$j + 3] =~ /^wpv/)
-						{
-							$temp_hex .= "0D0A" . substr(ascii_to_hex($textfile_lines[$j + 3]), 0, -2);
-							$textfile_lines[$j + 3] = "";
-						}
+						$temp_hex .= "0D0A" . substr(ascii_to_hex($textfile_lines[$j + 1]), 0, -2);
+						$textfile_lines[$j + 1] = "";
+					}
+					elsif($textfile_lines[$j + 2] =~ /^wpv/)
+					{
+						$temp_hex .= "0D0A" . substr(ascii_to_hex($textfile_lines[$j + 2]), 0, -2);
+						$textfile_lines[$j + 2] = "";
+					}
+					elsif($textfile_lines[$j + 3] =~ /^wpv/)
+					{
+						$temp_hex .= "0D0A" . substr(ascii_to_hex($textfile_lines[$j + 3]), 0, -2);
+						$textfile_lines[$j + 3] = "";
 					}
 
-					if($k < scalar(@temp_hex_split) - 1)
-					{
-						$temp_hex .= "0D0A6D6968203030300D0A6D726E206E6F0D0A";
-					}
+					# if($textfile_lines[$j + 2] =~ /^wpv/)
+					# {
+					# 	$temp_hex .= "0D0A" . substr(ascii_to_hex($textfile_lines[$j + 2]), 0, -2);
+					# 	$textfile_lines[$j + 2] = "";
+					# }
+					# elsif($textfile_lines[$j + 3] =~ /^wpv/)
+					# {
+					# 	$temp_hex .= "0D0A" . substr(ascii_to_hex($textfile_lines[$j + 3]), 0, -2);
+					# 	$textfile_lines[$j + 3] = "";
+					# }
+				}
+
+				if($k < scalar(@temp_hex_split) - 1)
+				{
+					$temp_hex .= "0D0A6D6968203030300D0A6D726E206E6F0D0A";
+				}
+				else
+				{
+					$temp_hex .= "0D";
 				}
 			}
-			else
-			{
-				$temp_hex .= "0D";
-			}
+		}
+		else
+		{
+			$temp_hex .= "0D";
+		}
 
-			if($j < scalar(@textfile_lines) - 1)
-			{
-				my $l = $j + 1;
-				my $mih_pattern_found = 0;
+		if($j < scalar(@textfile_lines) - 1)
+		{
+			my $l = $j + 1;
+			my $mih_pattern_found = 0;
 
-				while($mih_pattern_found == 0 && $l < scalar(@textfile_lines) - 1)
+			while($mih_pattern_found == 0 && $l < scalar(@textfile_lines) - 1)
+			{
+				if($english_replacements{$l + 1} eq "" || !exists($english_replacements{$l + 1}))
 				{
-					if($english_replacements{$l + 1} eq "" || !exists($english_replacements{$l + 1}))
+					if($textfile_lines[$l] =~ /^mih 000/)
 					{
-						if($textfile_lines[$l] =~ /^mih 000/)
-						{
-							$mih_pattern_found = 1;
-						}
-						else
-						{
-							$l ++;
-						}
-					}
-					elsif($l > $j + 1)
-					{
-						print " -> Found a missing \"mih 000\" code: line $l\n";
-						$temp_hex .= "0A6D696820303030";
 						$mih_pattern_found = 1;
 					}
 					else
 					{
-						$mih_pattern_found = 1;
+						$l ++;
 					}
 				}
+				elsif($l > $j + 1)
+				{
+					$temp_hex .= "0A6D6968203030300D";
+					$mih_pattern_found = 1;
+
+					print " -> Found a missing \"mih 000\" code: line $l\n";
+				}
+				else
+				{
+					$mih_pattern_found = 1;
+				}
 			}
-
-			$full_file_hex .= $temp_hex;
-
-			$line_count_english ++;
 		}
+
+		$full_file_hex .= $temp_hex;
+		$line_count_english ++;
 	}
-	else
+	elsif(!exists($english_replacements{$j + 1}) && $textfile_lines[$j] ne "")
 	{
-		if($textfile_lines[$j] ne "")
+		if($j > 0)
 		{
-			if($j > 0)
-			{
-				$full_file_hex .= "0A";
-			}
-			
-			$full_file_hex .= ascii_to_hex($textfile_lines[$j]);
-
-			if($j == scalar(@textfile_lines) - 1)
-			{
-				$full_file_hex .= "0A";
-			}
-
-			$line_count_orig ++;
+			$full_file_hex .= "0A";
 		}
+
+		if($textfile_lines[$j] =~ /mtt 000 016 05/)
+		{
+			print " -> Set infinite question timer: line $j\n";
+
+			$textfile_lines[$j] =~ s/mtt 000 016 05/mit 000/g;
+		}
+		
+		$full_file_hex .= ascii_to_hex($textfile_lines[$j]);
+
+		if($j == scalar(@textfile_lines) - 1)
+		{
+			$full_file_hex .= "0A";
+		}
+
+		$line_count_orig ++;
 	}
 }
 
+my $line_count_total = () = $full_file_hex =~ /0D0A/gi;
 my @full_file_hex_array = split(//, $full_file_hex);
-
 &write_bytes(\@full_file_hex_array, "txt_new/" . $script_file_out);
 
-print " -> English dialog line count: $line_count_english\n";
-print " -> Total line count: " . ($line_count_orig + $line_count_english) . "\n";
+print " -> Original line count: $line_count_orig\n";
+print " -> New line count: $line_count_total\n";
+print " -> English dialog lines processed: $line_count_english\n";
 
 sub write_bytes
 {
@@ -182,7 +248,7 @@ sub generate_hex
 	$input =~ s/”/"/g;
 	$input =~ s/“/"/g;
 	$input =~ s/\.\.\./#/g;
-	(my $input_fold = $input) =~ s/#/##/g;
+	$input =~ s/#/##/g;
 	my %char_table;
 
 	open my $char_map_handle, '<', $char_map;
@@ -195,16 +261,13 @@ sub generate_hex
 		$char_table{$tmp_char_split[1]} = $tmp_char_split[0];
 	}
 
-	my @input_chars = split(//, $input);
-
-	my $folded_text = fold_text($input_fold, 26, {'soft_hyphen_threshold' => '0'});
+	my $folded_text = fold_text($input, 26, {'soft_hyphen_threshold' => '0'});
 	my @folded_text_array = split("\n", $folded_text);
 	my $hex_final;
 
 	for(my $i = 0; $i < scalar(@folded_text_array); $i ++)
 	{
 		$folded_text_array[$i] =~ s/^\s+|\s+$//g;
-		(my $display_text = $folded_text_array[$i]) =~ s/##/\.\.\./g;
 		(my $temp_text = $folded_text_array[$i]) =~ s/##/#/g;
 		my @folded_chars = split(//, $temp_text);
 		my $char_count = 0;
