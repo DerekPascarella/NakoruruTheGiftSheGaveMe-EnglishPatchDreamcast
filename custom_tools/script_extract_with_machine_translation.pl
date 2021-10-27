f1b2da04df0c6765c4d1971f384ce5d64f81b65f
Derek Pascarella <contact.pascarella@gmail.com>
2021-10-27 13:08:32 -0400

    Update script_extract_with_machine_translation.pl

--- custom_tools/script_extract_with_machine_translation.pl
+++ custom_tools/script_extract_with_machine_translation.pl
@@ -125,7 +125,7 @@ for(my $i = 0; $i < scalar(@script_files); $i ++)
 
 				my $japanese_text = Encode::decode("utf-8", $japanese_text);
 				my $http = HTTP::Tiny->new;
-				my $post_data = uri_encode("auth_key=361bf0c8-9eaa-dcc4-d610-d59c8324f6a4:fx&target_lang=EN-US&source_lang=JA&text=" . $japanese_text);
+				my $post_data = uri_encode("auth_key=XXXXXXXXXXXXXXXXXXXX&target_lang=EN-US&source_lang=JA&text=" . $japanese_text);
 				my $response = $http->get("https://api-free.deepl.com/v2/translate?" . $post_data);
 				my $english_translation = decode_json($response->{'content'})->{'translations'}->[0]->{'text'};
 				
