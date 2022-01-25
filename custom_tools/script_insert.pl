259ff677b97c4f9276a0933d760dc817bf0a6fd5
Derek Pascarella <contact.pascarella@gmail.com>
2022-01-25 09:54:23 -0500

    Add files via upload

--- custom_tools/script_insert.pl
+++ custom_tools/script_insert.pl
@@ -76,13 +76,17 @@ for(my $j = 0; $j < scalar(@textfile_lines); $j ++)
 		}
 
 		# If English text represents spoken dialog (e.g. "[Speaker] This is my line."), is a player-selectable
-		# response for an in-game question, or is part of a minigame score-result script file (e.g.
-		# "scd_rs01_h0119_(825)"), automatically use the default font.
+		# response for an in-game question, is a game over/retry prompt (e.g. "scd_51_dgo_(966)"), is the
+		# game over text from one of the endings (e.g. "scd_27_i_dc_(972)"), or is part of a minigame
+		# score-result script file (e.g. "scd_rs01_h0119_(825)"), automatically use the default font.
 		if(substr($english_replacements{$j + 1}, 0, 1) eq "["
 			|| $textfile_lines[$j + 1] =~ /^mit/ || $textfile_lines[$j + 1] =~ /^mtt/
 			|| $textfile_lines[$j + 2] =~ /^mit/ || $textfile_lines[$j + 2] =~ /^mtt/
 			|| $textfile_lines[$j + 3] =~ /^mit/ || $textfile_lines[$j + 3] =~ /^mtt/
-			|| $script_file =~ /^scd_rs0[0-9]_h/)
+			|| $script_file =~ /^scd_rs0[0-9]_h/
+			|| $script_file =~ /go_/
+			|| $script_file =~ /yn_/
+			|| ($script_file =~ /i_[d|p]c/ && $english_replacements{$j + 1} =~ /GAME OVER/))
 		{
 			$temp_hex .= &generate_hex("chars.txt", $english_replacements{$j + 1});
 		}
