b06d6aa95b1248c39c6f0c5e37fb379f25b27d1e
Derek Pascarella <contact.pascarella@gmail.com>
2022-03-25 11:09:13 -0400

    Add files via upload

--- custom_tools/script_insert.pl
+++ custom_tools/script_insert.pl
@@ -414,6 +414,9 @@ if($script_file =~ /^scd_rs0[0-9]_h/)
 	$full_file_hex =~ s/0D0A82B082BD82CC82CE82C582CD82CC0D0A/0D0A88C588A288C888CA88B388C988C80D0A/gi;
 }
 
+# Replace Nakoruru's mysterious prayer text placeholder with hex for actual characters.
+$full_file_hex =~ s/82AE82B0829F82B782A382B082B282A382B682B282D882D6/8474844683B583A683B383D283C583C48476845E82D882D6/gi;
+
 # Store new script file's line count.
 my $line_count_total = () = $full_file_hex =~ /0D0A/gi;
 
